8060e6483e798d25269b2c1bc1414826ddd95bc3
Ar on <aronbjarklind@gmail.com>
2022-11-02 11:44:48 +0000

    major update, smá javascript en virkar bara fyrir eina mynd núna, alveg að koma....

--- index.js
+++ index.js
@@ -47,7 +47,39 @@
 // }
 
 
-function resizeImage(img) {
-    img.style.width = "500px";
-    img.style.height = "500px";
-  }
+window.onload = function() { // add window.onload here and set it euqal to a function
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  
+  var img = document.getElementById('myImg');
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  
+
+  img.onclick = function() {
+    modal.style.display = "block";
+
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+
+  };
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("modal")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  
+
+}; // close the function
+
+
+
+
+
+
